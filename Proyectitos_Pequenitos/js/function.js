ef6ba557c0e54b92cf403e1539097f3da2ddee77
crazypurpleunicorn <santiago.amoros@estudiantat.upc.edu>
2022-11-28 21:24:32 +0100

    comit2, adding smaller project's folder

---
+++ Proyectitos_Pequenitos/js/function.js
@@ -0,0 +1,90 @@
+
+/*Description of returnArrayTimeToUnlock: 
+Dada una inversión 'x' que cuesta un determinado valor comprar: unlockPrice
+Y dado el salario mensual actual que tenemos: basicIncome
+...Nos vamos a imaginar lo siguiente. Estamos cansados de no ser ricos y como
+personas sabias que somos, sabemos que con un salario no llegamos a ningún sitio.
+Las curvas convencionales nos las pasamos por el forro.
+A nosotros nos gustan las rectas que se tuercen.
+Por ello decidimos que todo el dinero que ganemos lo vamos a ahorrar
+para invertirlo en una máquina mágica que cuesta unlockPrice $. Esta máquina 
+incrementará nuestro salario mensual en una cantidad determinda,monthlyIncrement(Por ejemplo, si compramos
+un párquing mágico(unlockPrice=14000$ y que siempre está alquilado))
+tendría un monthlyIncrement de 100$. Si tuvieras dos tu monthlyIncrement sería de 200$ sobre tu salari base.
+
+Queremos saber, cuánto tiempo necesitamos para poder comprar otro párquing mágico, teniendo en cuenta
+que todo lo que ganamos(baseIncome+incrementoMensual) va destinado a comprar el siguiente.
+Queremos estudiar cómo va decrementandose el tiempo necesario en meses que necesito para comprar un párquing,
+si mantenemos este religioso ahorro.
+
+Así pues las funciones  "returnArrayTimeToUnlock"
+devuelven un array con los tiempos necesarios para comprar el siguiente párquing
+de ahí el nombre timeToUnlock.
+
+Este array, lo hacemos crecer hasta que se cumpla una condición, por ejemplo,
+hasta que cobre 1500$ sin hacer nada de nada, mágicamente a través de nuestras inversiones.
+
+Otra opción es hacerlo crecer hasta que el tiempo para adquirir el párquing sea menor de 
+X meses.¿Cuánto tardaría yo en poder comprar un párquing cada dos meses?
+
+Así pues  returnArrayTimeToUnlockTillPassiveIncomeGreaterThan(), se corresponde a la primera opción.
+y          returnArrayTimeToUnlockTillTimeToUnlockIsEqualTo(), se corresponde a la segunda opción
+
+
+*/
+
+//Duda: en js para que sirve asignar una función a una variable
+//es una cosa bastante, especial, en qué casos se puede usar????
+
+
+function returnArrayTimeToUnlockTillTimeToUnlockIsEqualTo(unlockPrice,baseIncome,monthlyIncrement,desiredTimeToUnlock){//desiredTimeToUnlock is in months
+    //inicializaciones
+    var timeToUnlock= unlockPrice/baseIncome
+    var arrayOfTimesToUnlock=[]
+
+    //Calculate timeToUnlocks...untill. Basic income will become basic salary+monthly increment.
+    while (timeToUnlock>=desiredTimeToUnlock){
+        arrayOfTimesToUnlock.push(timeToUnlock)
+        baseIncome=baseIncome+monthlyIncrement
+        timeToUnlock=unlockPrice/baseIncome
+    }
+    return arrayOfTimesToUnlock
+}
+
+
+function returnArrayTimeToUnlockTillPassiveIncomeGreaterThan(unlockPrice,baseIncome,monthlyIncrement,desiredPassiveIncome){
+     //inicializaciones
+     var initialBasicIncome=baseIncome
+     var timeToUnlock= unlockPrice/baseIncome
+     var arrayOfTimesToUnlock=[]
+
+     var passiveIncome=baseIncome-initialBasicIncome
+
+     //CalculateTimeToUnlocks...untill. Base income will become basic salary+monthly increment
+     while(passiveIncome<desiredPassiveIncome){
+        arrayOfTimesToUnlock.push(timeToUnlock)
+        baseIncome=baseIncome+monthlyIncrement
+        timeToUnlock=unlockPrice/baseIncome
+        passiveIncome=baseIncome-initialBasicIncome
+        
+     }
+     return arrayOfTimesToUnlock
+
+}
+
+
+//Format function, this function formats the array data so it can be interpreted by the JSCharting library..
+// it's an array of x and y arrays :[[x1,y1],[x2,y2],[x3,y3]...]
+
+function formatVector(array){
+    var formattedArray=[]
+    for (let i = 0; i < array.length; i++){
+        var x=i
+        var y=array[i]
+        var xyArray=[x,y]
+        formattedArray.push(xyArray)
+        console.log(formattedArray)
+
+    }
+    return formattedArray
+}
---
+++ Proyectitos_Pequenitos/js/index.js
@@ -0,0 +1,83 @@
+// JS 
+
+
+/*SETTERS...i am interseted in being able to change "var vector" since it is the vector used to display the graph
+on the div element. The user when has its input parameters ready will click on 'btn1' where it says plot1 and 
+a new graph created from this parameter should appear...To do that "vector" should be able to change every time
+I click on this button. So i will define a function setVector at the end, to achieve this*/ 
+
+var colors = JSC.getPalette('default'); 
+var colorText = function(txt, colorIndex) { 
+  return ( 
+    '<span style="color:' + 
+    colors[colorIndex] + 
+    '"><b>' + 
+    txt + 
+    '</b></span>'
+  ); 
+}; 
+var titleText = 
+  'Monthly ' + 
+  colorText('Purchases', 0) + 
+  ' vs. ' + 
+  colorText('Rent', 1);
+var vector=[ [1, 29.9], [2, 97.5], [3, 110.4], [4, 129.2], [5, 144.0], [6, 176.0]]
+  
+var chart = JSC.chart('chartDiv', { 
+  debug: true, 
+  type: 'area', 
+  title_label_text: titleText, 
+  legend_visible: false, 
+  yAxis: { 
+    formatString: 'c', 
+    scale_type: 'stacked'
+  }, 
+  xAxis: { 
+    crosshair_enabled: true, 
+    //scale: { type: 'time' } 
+  }, 
+  defaultSeries: { 
+    shape: { 
+      opacity: 0.6, 
+      /* Dynamic gradient that will work with any color series */
+      fill: ['lightenMore', '#f1f1f1', 90] 
+    }, 
+    defaultPoint_marker: { 
+      fill: 'white', 
+      type: 'circle', 
+      outline: { width: 1 } 
+    } 
+  }, 
+  
+  series: [ 
+    { 
+      name: 'Purchases', 
+      points: vector //[ 
+        
+        /*[1, 29.9], 
+        [2, 97.5], 
+        [3, 110.4], 
+        [4, 129.2], 
+        [5, 144.0], 
+        [6, 176.0] */
+      //] 
+    }, 
+    { 
+      name: 'Rent', 
+      points: [ 
+        [1, 86.9], 
+        [2, 79.5], 
+        [3, 95.4], 
+        [4, 97.2], 
+        [5, 123.0], 
+        [6, 111.0] 
+      ] 
+    } 
+  ] })
+
+
+  //not exactly a setter since vector is a global variable...This function changes the value of the 
+  //global variable 'vector'
+  function setVector(array){
+    vector=array
+}
